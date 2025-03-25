b7726ff6d7be12084bb02f581c20c348acd09ca3
Kankaanpää Aki A S <kankaki@cubbli22-gold.local>
2025-03-25 23:47:07 +0200

    Viikko 2

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,5 +1,5 @@
 import unittest
-from maksukortti import Maksukortti
+from maksukortti import Maksukortti # type: ignore
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
@@ -7,3 +7,29 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_rahan_talletus_OK(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_raha_lataa(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.0)
+
+    def test_rahaa_on_tarpeeksi_nostoon(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+
+    def test_rahat_eivät_riitä(self):
+        self.maksukortti.ota_rahaa(2000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_nosto_True(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(500))
+    
+    def test_nosto_False(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(2000))
+
+    def test_standardi_string(self):
+        kortti = Maksukortti(1500)
+        print(kortti)
+        self.assertEqual(str(kortti),"Kortilla on rahaa 15.00 euroa")
